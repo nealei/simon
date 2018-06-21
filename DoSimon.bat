817ff024722c38d6071eb19261921aa4ceb53808
nealei <39118292+nealei@users.noreply.github.com>
2018-06-21 21:38:06 +1000

    Update usage

--- DoSimon.bat
+++ DoSimon.bat
@@ -1,13 +1,13 @@
 @echo off
 :: 
-:: Usage: GoSimon [Roadfile (without extension) [Trafficfile(without extension) [RoadSec]]] [^> SimonSays.bat]
+:: Usage: DoSimon [Roadfile (without extension) [Trafficfile (without extension) [RoadSec]]] [^> DoSimon.bat]
 ::
-:: By Neale Irons Version 03/06/2018 (CC BY-SA 4.0)
+:: By Neale Irons Version 21/06/2018 (CC BY-SA 4.0)
 
 for %%a in ("%cd%") do set "CurDir=%%~nxa"
-if exist "Trarr trf Values.xlsx" python ..\make_trf.py "Trarr trf Values%3.xlsx"
 if exist ..\pypath.bat call ..\pypath
-call MakeSimonSetup > MakeSimonSetup.log
+if exist "TrarrSetupTRF%3.xlsx" python ..\make_trf.py "TrarrSetupTRF%3.xlsx"
+call MakeSimonSetup%3 > MakeSimonSetup%3.log
 cd ..
 echo .
 echo Please wait - simulating all traffic on all road options ...
