007b175999a4c47f91fe1c70024ec3bc79e407be
nealei <39118292+nealei@users.noreply.github.com>
2018-06-30 07:39:37 +1000

    No echo

--- mc.bat
+++ mc.bat
@@ -1,2 +1,2 @@
-@Adjust to installed path
-%windir%\System32\cmd.exe "/K" %USERPROFILE%\AppData\Local\Continuum\miniconda2\Scripts\activate.bat %USERPROFILE%\AppData\Local\Continuum\miniconda2
+@rem User to adjust this file to installed path
+@%windir%\System32\cmd.exe "/K" %USERPROFILE%\AppData\Local\Continuum\miniconda2\Scripts\activate.bat %USERPROFILE%\AppData\Local\Continuum\miniconda2
