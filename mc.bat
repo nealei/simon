131bf73e20f23be54b5ba829743ffb47735854aa
nealei <39118292+nealei@users.noreply.github.com>
2018-07-02 00:09:06 +1000

    Add files via upload

---
+++ mc.bat
@@ -0,0 +1,2 @@
+@rem User to adjust this file to installed path
+@%windir%\System32\cmd.exe "/K" %USERPROFILE%\AppData\Local\Continuum\miniconda2\Scripts\activate.bat %USERPROFILE%\AppData\Local\Continuum\miniconda2
