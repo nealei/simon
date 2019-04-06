70e5ff417bb066698324c87e8aa729ed4b291e3f
nealei <39118292+nealei@users.noreply.github.com>
2019-04-06 16:43:52 +1100

    Migrate to Python V3

--- mc.bat
+++ mc.bat
@@ -1,2 +1,2 @@
 @rem User to adjust this file to installed path
-@%windir%\System32\cmd.exe "/K" %USERPROFILE%\AppData\Local\Continuum\miniconda2\Scripts\activate.bat %USERPROFILE%\AppData\Local\Continuum\miniconda2
+@%windir%\System32\cmd.exe "/K" %USERPROFILE%\AppData\Local\Continuum\miniconda3\Scripts\activate.bat %USERPROFILE%\AppData\Local\Continuum\miniconda3
